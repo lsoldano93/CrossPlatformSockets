c75732d231e9f8bd9ee2c4ff6c0e0a3df48ca16c
Luke Soldano <lsoldano93@gmail.com>
2022-03-28 23:38:19 -0500

    Minor changes, not up and running yet

---
+++ src/common/ITcpStream.hpp
@@ -0,0 +1,11 @@
+#ifndef I_TCP_STREAM_H
+#define I_TCP_STREAM_H
+
+
+class ITcpStream
+{
+
+};
+
+
+#endif // I_TCP_STREAM_H
--- tests/Test_TcpStream.cpp
+++ tests/Test_TcpStream.cpp
@@ -1,7 +1,10 @@
 #include <gtest/gtest.h>
 
+#include "ITcpStream.h"
+
 // Demonstrate some basic assertions.
 TEST(TcpStream, BasicAssertions) {
+   std::cout << "this works" << std::endl;
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
