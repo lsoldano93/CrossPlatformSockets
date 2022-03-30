be9b658b91cf006282f7c46f9bd0f0bba3ba4652
Luke Soldano <lsoldano93@gmail.com>
2022-03-30 16:39:49 -0500

    Build and run unit tests script fully functional

--- scripts/BuildAndRunTests.py
+++ scripts/BuildAndRunTests.py
@@ -1,14 +1,39 @@
 
 from os import remove
 from pathlib import Path
+from subprocess import Popen, PIPE, STDOUT
+from sys import stdout
+from time import sleep
 
 # Constants
 LOG_DELIMITER = "----------------------------------------------------------"
 
 BUILD_DIR_NAME = "build"
+UNIT_TESTS_EXE_NAME = "CrossPlatformSockets_tests.exe"
+
 CROSS_PLATFORM_SOCKETS_PATH = Path(__file__).parent.parent
 CROSS_PLATFORM_SOCKETS_BUILD_PATH = CROSS_PLATFORM_SOCKETS_PATH.joinpath(BUILD_DIR_NAME)
+CROSS_PLATFORM_SOCKETS_TEST_EXE_PATH = Path(f"{CROSS_PLATFORM_SOCKETS_BUILD_PATH}/tests/Debug/{UNIT_TESTS_EXE_NAME}")
 
+def stream_process_output(process):
+   """
+   Polls the running process for std output such that a running subprocess can have its output
+   streamed to the console this Python script was run from
+   """
+   process_still_running = process.poll() is None
+   for line in process.stdout:
+      stdout.write(line.decode("utf-8")) # stdout does not use a new line like print
+   return process_still_running
+
+def exit_on_failure():
+   """
+   Function to call when this script fails, simply prints expected errors and ending strings
+   """
+   print("xx Exiting CrossPlatformSockets BuildAndRunTests")
+   print(f"{LOG_DELIMITER}\n")
+   exit()
+
+# Start script
 print(f"\n{LOG_DELIMITER}")
 print("** Running CrossPlatformSockets BuildAndRunTests...")
 print(f"-- Using paths:\n--> Parent path: {CROSS_PLATFORM_SOCKETS_PATH}\n--> Build path: {CROSS_PLATFORM_SOCKETS_BUILD_PATH}")
@@ -35,4 +60,62 @@ if look_for_cmake_cache:
       remove(cmake_cache_path)
 
 # Run CMake and include unit tests
+print("-- Running CMake on project...")
+print(f"{LOG_DELIMITER}\n")
 
+cmake_command_arguments = ["cmake", 
+                           "-DBUILD_CROSS_PLATFORM_SOCKETS_UNIT_TESTS=ON", 
+                           "-S",
+                           CROSS_PLATFORM_SOCKETS_PATH,
+                           "-B",
+                           CROSS_PLATFORM_SOCKETS_BUILD_PATH]
+process = Popen(cmake_command_arguments, stdout=PIPE, stderr=STDOUT)
+
+# Stream output from sub process while it's still alive
+while stream_process_output(process):
+   sleep(0.1)
+
+# Check results
+print(f"\n{LOG_DELIMITER}")
+print("** CrossPlatformSockets continuing...")
+print("-- Checking outcome of prior CMake attempt")
+
+return_code = process.poll()
+if return_code is None or 0 != return_code:
+   print("--> CMake command failed!")
+   exit_on_failure()
+
+print("--> CMake command succeeded!")
+
+# Now run build the project
+print("-- Building project...")
+print(f"{LOG_DELIMITER}\n")
+
+build_arguments = ["cmake", "--build", CROSS_PLATFORM_SOCKETS_BUILD_PATH]
+process = Popen(build_arguments, stdout=PIPE, stderr=STDOUT)
+
+# Stream output from sub process while it's still alive
+while stream_process_output(process):
+   sleep(0.1)
+
+# Check results
+print(f"\n{LOG_DELIMITER}")
+print("** CrossPlatformSockets continuing...")
+print("-- Checking outcome of build attempt")
+
+return_code = process.poll()
+if return_code is None or 0 != return_code:
+   print("--> Build failed!")
+   exit_on_failure()
+
+print("--> Build succeeded!")
+
+# Run unit tests
+print("-- Running unit tests...")
+print(f"{LOG_DELIMITER}\n")
+
+process = Popen([CROSS_PLATFORM_SOCKETS_TEST_EXE_PATH], stdout=PIPE, stderr=STDOUT)
+
+# Stream output from sub process while it's still alive
+while stream_process_output(process):
+   sleep(0.1)
